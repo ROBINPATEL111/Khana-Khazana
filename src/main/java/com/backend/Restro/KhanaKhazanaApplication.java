0860d2c34550a3114e60631ab698f0b6bdf3ce74
Robin Patel <rp2380126@gmail.com>
2025-08-08 13:08:28 +0530

    initial commit

---
+++ src/main/java/com/backend/Restro/KhanaKhazanaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Restro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhanaKhazanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhanaKhazanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/Restro/KhanaKhazanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Restro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhanaKhazanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
